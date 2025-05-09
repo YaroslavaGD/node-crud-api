4d6cf6f40922684900e239a953c5b7b96652b392
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-05-09 23:22:01 +0200

    refactor: add parseBody in helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,4 @@
-import { ServerResponse } from 'node:http';
+import { IncomingMessage, ServerResponse } from 'node:http';
 
 export const CODES = {
   OK: 200,
@@ -20,4 +20,22 @@ export const MESSAGES = {
 export function setHeader(res: ServerResponse, statusCode: (typeof CODES)[keyof typeof CODES]) {
   res.statusCode = statusCode;
   res.setHeader('Content-Type', 'application/json');
+}
+
+export function parseBody<T>(req: IncomingMessage): Promise<T> {
+  return new Promise((resolve, reject) => {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      try {
+        const parsed = JSON.parse(body);
+        resolve(parsed);
+      } catch (error) {
+        reject(error);
+      }
+    });
+    req.on('error', reject);
+  });
 }
